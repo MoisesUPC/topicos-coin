4ac05f22fb26f884ecafcec6512f017907a3388f
Moisés Cumpa <jose.cumpa@upc.edu.pe>
2025-10-03 22:34:26 -0500

    Update main.js
    Resolve some error/warning when no x property is in config variable.

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ const config = {
         default: 'arcade',
         arcade: {
             debug: false,
-            gravity: { y: 400 }
+            gravity: { x:0, y: 400 }
         }
     },
     scale: {
